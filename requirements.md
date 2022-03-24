6e892c735134113357213a180f21caac7549e428
vignesh203 <vigneshtnd1@gmail.com>
2022-03-24 17:21:33 +0530

    new add

---
+++ requirements.md
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+requirements
+aws ami
+aws keypai
+aws vpc with subnet
+aws security groups
+aws env (access key and keyid)
---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "VPC_ID" {
+  default = "vpc-0d847b18154525fed"
+}
+variable "SUBNET_ID" {
+  default = "subnet-07c49eaf9c064216b"
+}
+variable "PROJECT" {
+  default = "nagu"
+}
